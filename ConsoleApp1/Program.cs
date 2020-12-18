8bfeb0629b66adb5715378147e104a60007b9ef7
PiyushTyagiBDAT <71528379+PiyushTyagiBDAT@users.noreply.github.com>
2020-12-19 00:44:24 +0530

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var total = int.Parse(Console.ReadLine());
+            for (int a0 = 0; a0 < total; a0++)
+            {
+                var tokens_n = Console.ReadLine().Split(' ');
+                var cancellationThreshold = int.Parse(tokens_n[1]);
+                var a_temp = Console.ReadLine().Split(' ');
+                var studentEntryTimings = Array.ConvertAll(a_temp, Int32.Parse);
+                var onTimeStudents = 0;
+                foreach (var studentTiming in studentEntryTimings)
+                {
+                    if (studentTiming <= 0)
+                        onTimeStudents++;
+
+                    if (onTimeStudents == cancellationThreshold)
+                        break;
+                }
+
+                if (onTimeStudents < cancellationThreshold)
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+
+            }
+        }
+    }
+}
